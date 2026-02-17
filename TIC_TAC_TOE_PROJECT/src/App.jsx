603f11b5ec57a1038e65d79e519a67646e24af22
KimSooyeon <rtd710@naver.com>
2026-02-17 14:42:15 +0900

    fix: player

--- TIC_TAC_TOE_PROJECT/src/App.jsx
+++ TIC_TAC_TOE_PROJECT/src/App.jsx
@@ -1,6 +1,21 @@
 
 function App() {
-  return <p>Coming soon...</p>;
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player-name">Player 1</span>
+          <span className="plater-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">Player 2</span>
+          <span className="plater-symbol">O</span>
+        </li>
+      </ol>
+      GAME BOARD
+    </div>
+    LOG
+  </main>;
 }
 
 export default App;
